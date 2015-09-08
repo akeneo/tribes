81d0a05a452b88faa6022931d5910595477b6f49
willy-ahva <willy.mesnage@akeneo.com>
2015-09-08 10:43:02 +0200

    PING-2: Rename DefaultController to HomePage

--- src/Akeneo/Bundle/FrontofficeBundle/Controller/HomePageController.php
+++ src/Akeneo/Bundle/FrontofficeBundle/Controller/HomePageController.php
@@ -3,23 +3,24 @@
 namespace Akeneo\Bundle\FrontofficeBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
 
 /**
- * Class DefaultController.
+ * Controller for Home Page
  *
- * @author    Clement Gautier <clement.gautier@akeneo.com>
+ * @author    Willy Mesnage <willy.mesnage@akeneo.com>
  * @copyright 2015 Akeneo SAS (http://www.akeneo.com)
  * @license   http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
  */
-class DefaultController extends Controller
+class HomePageController extends Controller
 {
     /**
      * @Route
      * @Template
      *
-     * @return \Symfony\Component\HttpFoundation\Response
+     * @return Response
      */
     public function indexAction()
     {
