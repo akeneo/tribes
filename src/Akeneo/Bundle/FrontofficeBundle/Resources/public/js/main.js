707034279e0baf3457626fb45ab942fe858e5e8e
Clement Gautier <clement.gautier@akeneo.com>
2015-09-11 01:51:41 +0200

    wip

--- src/Akeneo/Bundle/FrontofficeBundle/Resources/public/js/main.js
+++ src/Akeneo/Bundle/FrontofficeBundle/Resources/public/js/main.js
@@ -1,3 +1,8 @@
 $(function () {
     $('.select2').select2();
+
+    $("#create_event_btn").click(function(event) {
+        event.preventDefault();
+        $(window).scrollTo($("#add_event_form"), 1000);
+    });
 });
