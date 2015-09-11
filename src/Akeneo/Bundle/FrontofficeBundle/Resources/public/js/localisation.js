f112c556d6080e6a4690ef7509e6b3afff5354a7
Remy Betus <remy.betus@akeneo.com>
2015-09-11 09:19:30 +0200

    Added autocomplete field for place filtering

---
+++ src/Akeneo/Bundle/FrontofficeBundle/Resources/public/js/localisation.js
@@ -0,0 +1,48 @@
+
+var placeSearch, autocomplete;
+
+function initAutocomplete() {
+    // Create the autocomplete object, restricting the search to geographical
+    // location types.
+    autocomplete = new google.maps.places.Autocomplete(
+        /** @type {!HTMLInputElement} */(document.getElementById('autocomplete')),
+        {types: ['geocode']});
+
+    // When the user selects an address from the dropdown, populate the address
+    // fields in the form.
+    autocomplete.addListener('place_changed', fillInAddress);
+}
+
+// [START region_fillform]
+function fillInAddress() {
+    // Get the place details from the autocomplete object.
+    var place = autocomplete.getPlace();
+    var location = place.geometry.location;
+
+    document.getElementById('filter_longitude').value = location.lng();
+    document.getElementById('filter_latitude').value = location.lat();
+}
+// [END region_fillform]
+
+// [START region_geolocation]
+// Bias the autocomplete object to the user's geographical location,
+// as supplied by the browser's 'navigator.geolocation' object.
+function geolocate() {
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(function(position) {
+            var geolocation = {
+                lat: position.coords.latitude,
+                lng: position.coords.longitude
+            };
+            var circle = new google.maps.Circle({
+                center: geolocation,
+                radius: position.coords.accuracy
+            });
+            autocomplete.setBounds(circle.getBounds());
+        });
+    }
+}
+$(function () {
+    initAutocomplete();
+});
+
--- src/Akeneo/Bundle/FrontofficeBundle/Resources/public/js/map.js
+++ src/Akeneo/Bundle/FrontofficeBundle/Resources/public/js/map.js
@@ -6,7 +6,24 @@ function initMap() {
         scrollwheel: false
     });
 
-    $.getJSON('/app_dev.php/api/events')
+    console.log('initializing map');
+    loadMap();
+    console.log('initializing map done');
+}
+
+function loadMap(latitude, longitude, from) {
+    var url = '/app_dev.php/api/events';
+    if (typeof latitude !== 'undefined') {
+        url += '&latitude=' + latitude;
+    }
+    if (typeof longitude !== 'undefined') {
+        url += '&longitude=' + longitude;
+    }
+    if (typeof from !== 'undefined') {
+        url += '&from=' + from;
+        url += '&to=+3 weeks';
+    }
+    $.getJSON(url)
         .then(function(data) {
             return $.Deferred(function( defer ) {
                 $.get('/bundles/akeneofrontoffice/templates/marker.mustache')
@@ -37,3 +54,17 @@ function initMap() {
 }
 
 google.maps.event.addDomListener(window, 'load', initMap);
+
+$("#create_event_btn").click(function(event) {
+    event.preventDefault();
+    $(window).scrollTo($("#add_event_form"), 1000);
+});
+
+$('#form_filter').submit(function(event) {
+   event.preventDefault();
+    latitude = $('#filter_latitude').val();
+    longitude = $('#filter_longitude').val();
+    date = $('#filter_date').val();
+
+    loadMap(latitude, longitude, date);
+});
